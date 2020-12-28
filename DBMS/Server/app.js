68d9eff1fbdc38b397336bbe8e208da91fb362b6
trevioncannon <helios.yamet@gmail.com>
2020-12-29 00:14:47 +0530

    Update app.js

--- DBMS/Server/app.js
+++ DBMS/Server/app.js
@@ -21,19 +21,7 @@ const app= express();
 const { Http2ServerRequest } = require("http2");
 app.use(express.json())
 
-app.get("/", (req, res) =>  res.send('<h1>Login:</h1><form action ="/login" method="POST"><h4>First Name</h4><input type="text" id="fname" name="fname"><br><h4>Last Name</h4><input type="text" id="lname" name="lname"><br><button type="submit">Enter</button></form>'));
 
-app.post("/login",(req,res,next)=>{
-    console.log(req.body['fname']);
-    
-    
-    
-    knex('employee').insert({Names: req.body['fname']}).then(console.log("written")).catch(e=>console.log(e))
-    
-
-
-
-    res.send('<h1> You logged in</h1><h2>Hello '+req.body['fname'])
     
 })
 
